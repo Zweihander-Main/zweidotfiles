77b2350e415fd4fecfe24b4e2f9e0e437599de48
Zweihänder <zweidev@zweihander.me>
2020-12-06 00:13:45 -0500

    feat(.doom.d): remove archive from ivy search

--- dot_doom.d/config.el
+++ dot_doom.d/config.el
@@ -4,36 +4,6 @@
 ;; Place your private configuration here! Remember, you do not need to run 'doom
 ;; sync' after modifying this file!
 
-;; Here are some additional functions/macros that could help you configure Doom:
-;;
-;; - `load!' for loading external *.el files relative to this one
-;; - `use-package!' for configuring packages
-;; - `after!' for running code after a package has loaded
-;; - `add-load-path!' for adding directories to the `load-path', relative to
-;;   this file. Emacs searches the `load-path' when you load packages with
-;;   `require' or `use-package'.
-;; - `map!' for binding new keys
-;;
-;; To get information about any of these functions/macros, move the cursor over
-;; the highlighted symbol at press 'K' (non-evil users must press 'C-c c k').
-;; This will open documentation for it, including demos of how they are used.
-;;
-;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
-;; they are implemented.
-
-;; Doom exposes five (optional) variables for controlling fonts in Doom. Here
-;; are the three important ones:
-;;
-;; + `doom-font'
-;; + `doom-variable-pitch-font'
-;; + `doom-big-font' -- used for `doom-big-font-mode'; use this for
-;;   presentations or streaming.
-;;
-;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
-;; font string. You generally only need these two:
-;; (setq doom-font (font-spec :family "monospace"i :size 12 :weight 'semi-light)
-;;       doom-variable-pitch-font (font-spec :family "sans" :size 13))
-
 ;;; Code:
 
 
@@ -83,6 +53,7 @@
       version-control t ;; number backups
       vc-make-backup-files t ;;backup version controlled files
       auto-save-interval 3)
+
 
 ;; ===============
 ;;   Directories
@@ -121,6 +92,14 @@
   "File for one-off tasks that should be done immediately or are currently being worked on.")
 
 (setq +org-capture-todo-file zwei/org-agenda-todo-file)
+
+
+;; =====
+;;  Ivy
+;; =====
+
+;; Set search to ignore archives
+(setq counsel-find-file-ignore-regexp "\\(?:^[#.]\\)\\|\\(?:[#~]$\\)\\|\\(?:^Icon?\\)\\|\\.org_archive")
 
 
 ;; ===============
