9cc7639d77c138c529cf7d779585d556425cfaae
Zweihänder <zweidev@zweihander.me>
2020-12-11 22:51:07 -0500

    feat(.doom.d): enable debugger and editorconfig

--- dot_doom.d/init.el
+++ dot_doom.d/init.el
@@ -85,10 +85,10 @@
 
        :tools
        ;;ansible
-       ;;debugger          ; FIXME stepping through code, to help you add bugs
+       debugger          ; FIXME stepping through code, to help you add bugs
        ;;direnv
        ;;docker
-       ;;editorconfig      ; let someone else argue about tabs vs spaces
+       editorconfig      ; let someone else argue about tabs vs spaces
        ;;ein               ; tame Jupyter notebooks with emacs
        (eval +overlay)     ; run code, run (also, repls)
        ;;gist              ; interacting with github gists
