426d8c5a9ec622db048ae0d7810d39c581937752
Zweihänder <zweidev@zweihander.me>
2021-01-04 01:03:19 -0500

    fix(.doom.d.): properly load variable pitch

--- dot_doom.d/+ui.el
+++ dot_doom.d/+ui.el
@@ -7,16 +7,11 @@
 ;;;
 ;;; Code:
 
-;; Load variable pitch library for variable pitch mode in org.
-(load! "lisp/org-variable-pitch")
-
-
 (setq doom-theme 'doom-tomorrow-night
       doom-font (font-spec :family "Iosevka SS09 Extended" :size 15)
       doom-unicode-font (font-spec :family "Iosevka Term SS09 Extended" :size 15)
       doom-variable-pitch-font (font-spec :family "Iosevka Aile" :size 16)
       doom-serif-font doom-variable-pitch-font
-      org-variable-pitch-fixed-font "Iosevka SS09 Extended"
       org-ellipsis "▼"
       display-line-numbers-type t
       line-spacing 0.1
@@ -29,14 +24,23 @@
 (doom-themes-org-config)
 (doom-init-extra-fonts-h)
 
-(add-hook! 'org-mode-hook #'(+org-pretty-mode org-variable-pitch-minor-mode))
+(use-package! org-variable-pitch
+  :after org
+  :config
+  ;; Functions
 
-;; Fix issues with jit-lock in org-capture buffer when variable-pitch fonts are used:
-(defun zwei/disable-org-variable-pitch-minor-mode ()
-  "Disable variable pitch mode."
-  (org-variable-pitch-minor-mode -1))
+  ;; Fix issues with jit-lock in org-capture buffer when variable-pitch fonts are used:
+  (defun zwei/disable-org-variable-pitch-minor-mode ()
+    "Disable variable pitch mode."
+    (org-variable-pitch-minor-mode -1))
 
-(add-hook! 'org-capture-mode-hook #'(zwei/disable-org-variable-pitch-minor-mode))
+  ;; Hooks
+  (add-hook! 'org-mode-hook #'(+org-pretty-mode org-variable-pitch-minor-mode))
+  (add-hook! 'org-capture-mode-hook #'(zwei/disable-org-variable-pitch-minor-mode))
+
+  ;; Vars
+  (setq org-variable-pitch-fixed-font "Iosevka SS09 Extended"))
+
 
 (add-hook! 'org-agenda-mode-hook #'(solaire-mode hl-line-mode))
 
