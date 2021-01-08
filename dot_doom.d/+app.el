cfaba200eb9dbed785d4e17a197723c9882c93a8
Zweihänder <zweidev@zweihander.me>
2021-01-08 00:36:13 -0500

    fix(.doom.d): add in better wsl copy/paste method

--- dot_doom.d/+app.el
+++ dot_doom.d/+app.el
@@ -99,13 +99,28 @@
 ;;  OS Specific
 ;; =============
 
-;; Enable emacs to open links in Windows
 (let ((cmd-exe "/mnt/c/Windows/System32/cmd.exe")
       (cmd-args '("/c" "start")))
   (when (file-exists-p cmd-exe)
+;; Enable emacs to open links in Windows
     (setq browse-url-generic-program cmd-exe
           browse-url-generic-args cmd-args
-          browse-url-browser-function 'browse-url-generic)))
+          browse-url-browser-function 'browse-url-generic))
+
+  ;; Copy in WSL
+  (defun wsl-copy (start end)
+    (interactive "r")
+    (shell-command-on-region start end "clip.exe")
+    (deactivate-mark))
+
+  ;; Paste in WSL
+  (defun wsl-paste ()
+    (interactive)
+    (let ((clipboard
+           (shell-command-to-string "powershell.exe -command 'Get-Clipboard' 2> /dev/null")))
+      (setq clipboard (replace-regexp-in-string "\r" "" clipboard))
+      (setq clipboard (substring clipboard 0 -1))
+      (insert clipboard))))
 
 
 ;; Local Variables:
