5b65111d3202982226159e07242a044c81343999
Zweihänder <zweidev@zweihander.me>
2021-09-01 15:17:39 -0400

    refactor(ohmyzsh): start removing duplicate library functions

--- private_dot_config/zsh/private_oh-my-zsh/lib/directories.zsh
+++ private_dot_config/zsh/private_oh-my-zsh/lib/directories.zsh
@@ -19,9 +19,6 @@ alias 7='cd -7'
 alias 8='cd -8'
 alias 9='cd -9'
 
-alias md='mkdir -p'
-alias rd=rmdir
-
 function d () {
   if [[ -n $1 ]]; then
     dirs "$@"
@@ -31,8 +28,3 @@ function d () {
 }
 compdef _dirs d
 
-# List directory contents
-alias lsa='ls -lah'
-alias l='ls -lah'
-alias ll='ls -lh'
-alias la='ls -lAh'
