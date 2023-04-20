628c27ffe373d0133211a4c9eaea4604c5c819db
Zweihänder <zweidev@zweihander.me>
2023-04-19 23:13:02 -0400

    feat(shell): fix zeta theme
    Built own theme to fix issues.

---
+++ private_dot_config/shell/.dir-locals.el
@@ -0,0 +1,8 @@
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+;; ((sh-mode . ((+format-with . :none))))
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+((sh-mode . ((lsp-warn-no-matched-clients . nil))))
