557fe35cf2b6570f5f8de8bbeb8553bafe65e265
Zweihänder <zweidev@zweihander.me>
2024-06-12 01:04:50 -0400

    fix(aethelweard): avoid using swap on sd card

--- nix/hosts/aethelweard/hardware-configuration.nix
+++ nix/hosts/aethelweard/hardware-configuration.nix
@@ -27,9 +27,7 @@
     fsType = "vfat";
   };
 
-  swapDevices = [
-    {device = "/dev/disk/by-uuid/a31cb015-4d66-4809-bf2a-79d9fb8b0135";}
-  ];
+  # No swap to avoid destroying sd device
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
