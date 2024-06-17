22a3c59a43dbb8019adbcbb772c7b787cfa4f7a9
Zweihänder <zweidev@zweihander.me>
2024-06-17 00:49:20 -0400

    fix(chromium): add in missing psd deps

--- nix/home/zwei/chromium/default.nix
+++ nix/home/zwei/chromium/default.nix
@@ -17,6 +17,43 @@
   };
   xdg.configFile."psd/psd.conf".source = ./psd.conf;
 
+  ## Temporary fix waiting on upstream
+  systemd.user = {
+    services = let
+      exe = "${pkgs.profile-sync-daemon}/bin/profile-sync-daemon";
+      envPath = lib.makeBinPath (with pkgs; [
+        rsync
+        kmod
+        findutils
+        gawk
+        gnugrep
+        gnused
+        coreutils
+        nettools
+        util-linux
+        profile-sync-daemon
+      ]);
+    in {
+      psd = {
+        Service = lib.mkForce {
+          Type = "oneshot";
+          RemainAfterExit = "yes";
+          ExecStart = "${exe} startup";
+          ExecStop = "${exe} unsync";
+          Environment = ["LAUNCHED_BY_SYSTEMD=1" "PATH=$PATH:${envPath}"];
+        };
+      };
+
+      psd-resync = {
+        Service = lib.mkForce {
+          Type = "oneshot";
+          ExecStart = "${exe} resync";
+          Environment = ["PATH=$PATH:${envPath}"];
+        };
+      };
+    };
+  };
+
   # For cache to RAM
   systemd.user.tmpfiles.rules = ["d /run/user/1000/chromium-cache"];
   home.file.".cache/chromium".source = config.lib.file.mkOutOfStoreSymlink "/run/user/1000/chromium-cache";
