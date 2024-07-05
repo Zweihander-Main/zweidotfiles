d9600fadf4f3f2b30c34397b028092bf104abe6f
Zweihänder <zweidev@zweihander.me>
2024-07-05 01:11:19 -0400

    feat(pipe-viewer): add in config

--- nix/home/zwei/pipe-viewer/default.nix
+++ nix/home/zwei/pipe-viewer/default.nix
@@ -10,13 +10,32 @@ in {
   home.packages = with pkgs; [
     pipe-viewer
     mplayer
+    # j2cli
   ];
 
   systemd.user.tmpfiles.rules = ["d ${homeDir}/vids/towatch"];
 
-  # TODO: ffmpeg, wget, ytdlp, players needs templating
+  # TODO: remove old config, port to desktop
   # TODO: music only option for config
 
-  xdg.configFile."pipe-viewer/pipe-viewer.conf".source = ./pipe-viewer.conf;
+  # Templating using jinja2?
 
+  # pkgs.runCommand
+  # "myscript"
+  # {
+  #   passAsFile = [
+  #     "paramsJson"
+  #   ];
+  #   paramsJson = builtins.toJSON {
+  #     items = [
+  #       "socks"
+  #       "towel"
+  #     ];
+  #   };
+  # }
+  # '''
+  #   ${pkgs.j2cli}/bin/j2 -f json ${./my-script.j2} "$paramsJsonPath" > "$out"
+  # ''
+
+  xdg.configFile."pipe-viewer/pipe-viewer.conf".source = ./pipe-viewer.conf;
 }
