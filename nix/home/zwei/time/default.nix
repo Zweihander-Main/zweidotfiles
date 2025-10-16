8ef55136ca65fdbfc6ca3033bc1d74bbce44cf8f
Zweihänder <zweidev@zweihander.me>
2025-10-16 16:45:26 -0400

    feat(time): push time cutoffs closer

--- nix/home/zwei/time/default.nix
+++ nix/home/zwei/time/default.nix
@@ -42,9 +42,9 @@ in {
 
     Timer = {
       OnCalendar = [
-        "Mon,Tue,Thu,Fri,Sat *-*-* 16:25,35" # 16:25, 16:35 except on Wed/Sun
-        "Wed *-*-* 17:10,20" # 17:10, 17:20 on Wed
-        "Sun *-*-* 16:00,10" # 16:00, 16:10 on Sun
+        "Mon,Tue,Thu,Fri,Sat *-*-* 16:45,55" # 16:45, 16:55 except on Wed/Sun
+        "Wed *-*-* 17:20,30" # 17:20, 17:30 on Wed
+        "Sun *-*-* 16:20,30" # 16:20, 16:30 on Sun
       ];
     };
 
@@ -68,9 +68,9 @@ in {
 
     Timer = {
       OnCalendar = [
-        "Mon,Tue,Thu,Fri,Sat,Sun *-*-* 16:40..55:0,15,30,45" # 16:45-55, every 15 secs except on Wed/Sun
-        "Wed *-*-* 17:25..30:0,15,30,45" # 17:25-30, every 15 secs on Wed
-        "Sun *-*-* 16:15..30:0,15,30,45" # 16:15-30, every 15 secs on Sun
+        "Mon,Tue,Thu,Fri,Sat,Sun *-*-* 17.00..15:0,15,30,45" # 17:00-15, every 15 secs except on Wed/Sun
+        "Wed *-*-* 17:40..45:0,15,30,45" # 17:40-45, every 15 secs on Wed
+        "Sun *-*-* 16:40..50:0,15,30,45" # 16:40-50, every 15 secs on Sun
       ];
       AccuracySec = "5s";
     };
